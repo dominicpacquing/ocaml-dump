be182cef0046a52dc0b800b0a57b778fd2521689
Paul Pacquing <dominicpacquing@gmail.com>
2017-09-16 16:40:10 +1000

    Add more list examples

--- src/lists.ml
+++ src/lists.ml
@@ -37,3 +37,46 @@ let rec drop n l =
     match l with
        [] -> l
     |  h :: t -> drop (n - 1) t ;;
+
+let rec evenElements l =
+  match l with
+   _ :: a :: t -> a :: evenElements t
+  | _ -> [] ;;
+
+let rec countTrue l total =
+  match l with
+    [] -> total
+  | h :: t -> countTrue t (if h = true then total + 1 else total) ;;
+
+let rec rev_accumulator l acc =
+  match l with
+  [] -> acc
+  | a :: t -> rev_accumulator t (a :: acc) ;;
+
+let rev l = rev_accumulator l [] ;;
+
+let createPalindrome l =
+  l @ rev l ;;
+
+let isPalindrome l m =
+  l = rev m ;;
+
+let rec dropLast l =
+  match l with
+    [] -> []
+  | [ a ] -> []
+  | h :: t -> h :: dropLast t ;;
+
+let rec member a l =
+  match l with
+   [] -> false
+  | h :: t -> if h = a then true else member a t ;;
+
+let rec make_set l =
+  match l with
+    [] -> []
+  | h :: t ->
+      if member h t = true then
+        make_set t
+      else
+        h :: make_set t ;;
