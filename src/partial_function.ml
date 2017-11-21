a6ca44cb347def370aeea9082d3f10d64cef32e1
Paul Pacquing <dominicpacquing@gmail.com>
2017-11-21 22:58:30 +1100

    Add more partial functions example

--- src/partial_function.ml
+++ src/partial_function.ml
@@ -22,3 +22,12 @@ signature: map fn l = (a -> b) -> a list -> b list
 map (map fn): a list list -> b list list
 *)
 let mapl fn ll = map (map fn) ll ;;
+
+let member_all x lsls =
+  let vals = map (member x) lsls in
+    not (member false vals );;
+
+let rdiv x y = y / x ;;
+
+let map_div_2 l =
+  map (rdiv 2) l ;;
